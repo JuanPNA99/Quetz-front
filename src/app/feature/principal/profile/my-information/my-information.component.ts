19241405ac02ba89c5d9b402dcf785d1027c6f96
JuanPNA99 <juanp.narvaez@gmail.com>
2021-12-05 22:58:57 -0500

    adding profile user information update

--- src/app/feature/principal/profile/my-information/my-information.component.ts
+++ src/app/feature/principal/profile/my-information/my-information.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UsersService } from 'src/app/core/services/users/users.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-my-information',
@@ -9,10 +12,27 @@ import { UsersService } from 'src/app/core/services/users/users.service';
 export class MyInformationComponent implements OnInit {
     @Input() userData!: any;
     infoMod: boolean = false;
-    constructor(private usersService: UsersService) {}
+    formUser!: FormGroup;
+    constructor(
+        private usersService: UsersService,
+        private formBuilder: FormBuilder,
+        private router: Router
+    ) {}
 
     ngOnInit() {
-        console.log(this.infoMod);
+        this.buildFormUser();
+    }
+
+    buildFormUser(): void {
+        this.formUser = this.formBuilder.group({
+            institucion_educativa: [this.userData.institucion_educativa],
+            idiomas: [this.userData.idiomas],
+            ubicacion: [this.userData.ubicacion],
+            facebookurl: [this.userData.facebookurl],
+            twitterurl: [this.userData.twitterurl],
+            youtubeurl: [this.userData.youtubeurl],
+            instagramurl: [this.userData.instagramurl],
+        });
     }
 
     modify() {
@@ -21,5 +41,40 @@ export class MyInformationComponent implements OnInit {
 
     cancelModify() {
         this.infoMod = false;
+    }
+
+    getData(): void {
+        this.usersService.profile().subscribe(
+            (res) => {
+                this.userData = res;
+                console.log(res);
+            },
+            (err) => {
+                console.log(err);
+            }
+        );
+    }
+
+    userUpdate(data: any) {
+        this.usersService.profileUpdate(data).subscribe(
+            (data) => {
+                console.log(data);
+                Swal.fire({
+                    title: 'Perfil Actualizado correctamente',
+                    icon: 'success',
+                });
+                this.getData();
+                this.infoMod = false;
+            },
+            (err) => {
+                console.log(err);
+                Swal.fire({
+                    title: 'Hubo un problema',
+                    icon: 'error',
+                });
+                this.getData();
+                this.infoMod = false;
+            }
+        );
     }
 }
